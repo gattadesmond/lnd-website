dabc1c467b009d0b99bf5a4ca0ed0f1ace0caf5c
loc.le4 <>
2025-09-24 17:38:18 +0700

    test(login): add log

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -19,47 +19,47 @@ export async function GET(request: NextRequest) {
   const decodedNext = getDecodedNext(searchParams.get("next") ?? "/");
   const redirectUrl = new URL(decodedNext, origin);
   console.log("🚀 ~ GET ~ redirectUrl:", redirectUrl);
-  if (!code) {
-    console.log("nocode");
-    return NextResponse.redirect(redirectUrl);
-  }
-  const supabase = await createClient();
-  const { error, data } = await supabase.auth.exchangeCodeForSession(code);
-  if (error) {
-    console.log("error");
-    return NextResponse.redirect(redirectUrl);
-  }
-  const { user } = data;
-  if (
-    !("custom_claims" in user.user_metadata) ||
-    !("hd" in user.user_metadata.custom_claims) ||
-    user.user_metadata.custom_claims.hd !== "mservice.com.vn"
-  ) {
-    await supabase.auth.admin.deleteUser(user.id);
-    const cookieStore = await cookies();
-    cookieStore.getAll().forEach(({ name }) => {
-      if (name.startsWith(process.env.NEXT_PUBLIC_SUPABASE_AUTH_STORAGE_KEY!)) {
-        cookieStore.delete(name);
-      }
-    });
-    cookieStore.set("auth_error", "domain_mismatch", {
-      httpOnly: false,
-      maxAge: 10,
-      path: "/",
-    });
+  // if (!code) {
+  //   console.log("nocode");
+  //   return NextResponse.redirect(redirectUrl);
+  // }
+  // const supabase = await createClient();
+  // const { error, data } = await supabase.auth.exchangeCodeForSession(code);
+  // if (error) {
+  //   console.log("error");
+  //   return NextResponse.redirect(redirectUrl);
+  // }
+  // const { user } = data;
+  // if (
+  //   !("custom_claims" in user.user_metadata) ||
+  //   !("hd" in user.user_metadata.custom_claims) ||
+  //   user.user_metadata.custom_claims.hd !== "mservice.com.vn"
+  // ) {
+  //   await supabase.auth.admin.deleteUser(user.id);
+  //   const cookieStore = await cookies();
+  //   cookieStore.getAll().forEach(({ name }) => {
+  //     if (name.startsWith(process.env.NEXT_PUBLIC_SUPABASE_AUTH_STORAGE_KEY!)) {
+  //       cookieStore.delete(name);
+  //     }
+  //   });
+  //   cookieStore.set("auth_error", "domain_mismatch", {
+  //     httpOnly: false,
+  //     maxAge: 10,
+  //     path: "/",
+  //   });
 
-    console.log("custom");
-    return NextResponse.redirect(redirectUrl);
-  }
-  const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-  const isLocalEnv = process.env.NODE_ENV === "development";
-  if (isLocalEnv || !forwardedHost) {
-    console.log("isLocalEnv");
-    // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-    return NextResponse.redirect(redirectUrl);
-  }
+  //   console.log("custom");
+  //   return NextResponse.redirect(redirectUrl);
+  // }
+  // const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+  // const isLocalEnv = process.env.NODE_ENV === "development";
+  // if (isLocalEnv || !forwardedHost) {
+  //   console.log("isLocalEnv");
+  //   // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+  //   return NextResponse.redirect(redirectUrl);
+  // }
 
-  console.log("end", process.env.NEXT_PUBLIC_DOMAIN, decodedNext);
+  // console.log("end", process.env.NEXT_PUBLIC_DOMAIN, decodedNext);
 
   return NextResponse.redirect(
     `${process.env.NEXT_PUBLIC_DOMAIN}${decodedNext}`,
