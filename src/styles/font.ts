8db34f2ec1427ca3c20649422c9b3c7f5381e8eb
TRẦN CÔNG HOÀNG TRỌNG - Product - Software Engineer II <trong.tran2@mservice.com.vn>
2025-10-02 12:16:04 +0700

    fix: add subsets for font

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,4 +1,4 @@
 import { Noto_Color_Emoji } from "next/font/google";
 
-const notoEmoji = Noto_Color_Emoji({ weight: "400" });
+const notoEmoji = Noto_Color_Emoji({ weight: "400", subsets: ["emoji"] });
 export { notoEmoji };
